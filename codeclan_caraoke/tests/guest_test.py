3c19cfef205fac3dad2b5241ddc1aad938db6bcc
Stefano Binando <sbinando@gmail.com>
2021-10-29 12:35:11 +0100

    * Creates class Guest in guest.py * Adds test values in guest_test.py * Imports class Guest in guest_test.py * Initialises class TestGuest SetUp with guests 1 and 11 * Initial tests to assert correct attribution of instance attributes: - test_guest_has_name — passed

--- codeclan_caraoke/tests/guest_test.py
+++ codeclan_caraoke/tests/guest_test.py
@@ -1,4 +1,22 @@
 import unittest
+from src.guest import Guest
+
+# Declaration of values to be used for tests
+guests = ["Mike", "Tom", "Susan", "Kate", "David", "Alex", "Robert", "Zoe", "Louise", "George", "Heather"]
 
 class TestGuest(unittest.TestCase):
-    pass
+    def setUp(self):
+        self.guest1 = Guest(guests[0])
+        self.guest2 = Guest(guests[1])
+        self.guest3 = Guest(guests[2])
+        self.guest4 = Guest(guests[3])
+        self.guest5 = Guest(guests[4])
+        self.guest6 = Guest(guests[5])
+        self.guest7 = Guest(guests[6])
+        self.guest8 = Guest(guests[7])
+        self.guest9 = Guest(guests[8])
+        self.guest10 = Guest(guests[9])
+        self.guest11 = Guest(guests[10])
+
+    def test_guest_has_name(self):
+        self.assertEqual(self.guest1.name, guests[0])
