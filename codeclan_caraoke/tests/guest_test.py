8c6a80aec9f9bb087f57a7a13d54067aaa9bbc07
Stefano Binando <sbinando@gmail.com>
2021-10-29 15:11:27 +0100

    guest.py amended to add wallet attribute guest_test.py and room_test.py amended to add values for wallet in objects guest 1 to 11

--- codeclan_caraoke/tests/guest_test.py
+++ codeclan_caraoke/tests/guest_test.py
@@ -6,17 +6,17 @@ guests = ["Mike", "Tom", "Susan", "Kate", "David", "Alex", "Robert", "Zoe", "Lou
 
 class TestGuest(unittest.TestCase):
     def setUp(self):
-        self.guest1 = Guest(guests[0])
-        self.guest2 = Guest(guests[1])
-        self.guest3 = Guest(guests[2])
-        self.guest4 = Guest(guests[3])
-        self.guest5 = Guest(guests[4])
-        self.guest6 = Guest(guests[5])
-        self.guest7 = Guest(guests[6])
-        self.guest8 = Guest(guests[7])
-        self.guest9 = Guest(guests[8])
-        self.guest10 = Guest(guests[9])
-        self.guest11 = Guest(guests[10])
+        self.guest1 = Guest(guests[0], 10)
+        self.guest2 = Guest(guests[1], 20)
+        self.guest3 = Guest(guests[2], 15)
+        self.guest4 = Guest(guests[3], 50)
+        self.guest5 = Guest(guests[4], 5)
+        self.guest6 = Guest(guests[5], 25)
+        self.guest7 = Guest(guests[6], 33)
+        self.guest8 = Guest(guests[7], 12)
+        self.guest9 = Guest(guests[8], 27)
+        self.guest10 = Guest(guests[9], 100)
+        self.guest11 = Guest(guests[10], 18)
 
     def test_guest_has_name(self):
         self.assertEqual(self.guest1.name, guests[0])
