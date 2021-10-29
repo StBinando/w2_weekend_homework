9a6ff3ccc566a223d339e2d996eff9221e45f94a
Stefano Binando <sbinando@gmail.com>
2021-10-29 09:30:38 +0100

    •Creates class Room in room.py •Adds test values in room_test.py •Imports class Room in room_test.py •Initialises class TestRoom SetUp with rooms 1 and 2 •room tests to assert correct attribution of instance attributes: - test_room_has_name — passed - test_room_has_guests — passed - test_room_is_empty — passed - test_room_has_songs — passed

---
+++ codeclan_caraoke/src/room.py
@@ -0,0 +1,5 @@
+class Room:
+    def __init__(self, name, guests, songs):
+        self.name = name
+        self.guests = guests
+        self.songs = songs
--- codeclan_caraoke/tests/room_test.py
+++ codeclan_caraoke/tests/room_test.py
@@ -1,4 +1,50 @@
 import unittest
+from src.room import Room
+
+# Declaration of values to be used for tests
+room_names = ["Rock", "Pop", "Punk", "Jazz", "Hard Rock", "Bossa Nova"]
+guests = ["Mike", "Tom", "Susan", "Kate", "David", "Alex", "Robert", "Zoe", "Louise", "George", "Heather"]
+songs = [
+    {
+        "title" : "Five Years",
+        "by" : "David Bowie"
+    },
+    {
+        "title" :"Kill the poor",
+        "by" : "Dead Kennedys"
+    },
+    {
+        "title" :"Sea and Sand",
+        "by" : "Nicola Conte"
+    },
+    {
+        "title" :"Águas de Março",
+        "by" : "Tom Jobim"
+    },
+    {
+        "title" :"Hells Bells",
+        "by" : "AC/DC"
+    },
+    {
+        "title" :"Paparazzi",
+        "by" : "Lady Gaga"
+    }
+]
+
 
 class TestRoom(unittest.TestCase):
-    pass
+    def setUp(self):
+        self.room1 = Room(room_names[0], guests[0:5], songs[0:3])
+        self.room2 = Room(room_names[1], [], songs[4:5])
+
+    def test_room_has_name(self):
+        self.assertEqual(self.room1.name, room_names[0])
+
+    def test_room_has_guests(self):
+        self.assertEqual(self.room1.guests, guests[0:5])
+
+    def test_room_is_empty(self):
+        self.assertEqual(self.room2.guests, [])
+
+    def test_room_has_songs(self):
+        self.assertTrue(len(self.room2.songs) > 0)
