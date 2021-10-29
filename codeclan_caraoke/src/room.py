3720158095342ab4a78737692d5f3bd98f0b3359
Stefano Binando <sbinando@gmail.com>
2021-10-29 14:30:54 +0100

    * In room.py methods added: - “check_in_1_guest” - “check_out_1_guest” - “check_out_all_guests_in_this_room” * In room_test.py tests added: - test_check_in_1_guest — passed - test_check_out_1_guest_ok — passed - test_check_out_1_guest_empty_room — passed - test_check_out_1_guest_is_not_here — passed - test_check_out_all_guests_in_this_room — passed - test_check_out_all_guests_in_empty_room — passed

--- codeclan_caraoke/src/room.py
+++ codeclan_caraoke/src/room.py
@@ -3,3 +3,18 @@ class Room:
         self.name = name
         self.guestlist = guestlist
         self.songlist = songlist
+
+    def check_in_1_guest(self, guest):
+        self.guestlist.append(guest)
+
+    def check_out_1_guest(self, guest):
+        if len(self.guestlist) == 0:
+            return "this room is empty"
+        elif not(guest in self.guestlist):
+            return "guest not in this room"
+        self.guestlist.remove(guest)
+
+    def check_out_all_guests_in_this_room(self):
+        if len(self.guestlist) == 0:
+            return "this room is already empty"
+        self.guestlist.clear()
--- codeclan_caraoke/tests/room_test.py
+++ codeclan_caraoke/tests/room_test.py
@@ -45,35 +45,35 @@ songs = [
 
 class TestRoom(unittest.TestCase):
     def setUp(self):
-        song1 = Song(songs[0]["title"], songs[0]["by"])
-        song2 = Song(songs[1]["title"], songs[1]["by"])
-        song3 = Song(songs[2]["title"], songs[2]["by"])
-        song4 = Song(songs[3]["title"], songs[3]["by"])
-        song5 = Song(songs[4]["title"], songs[4]["by"])
-        song6 = Song(songs[5]["title"], songs[5]["by"])
-        song7 = Song(songs[6]["title"], songs[6]["by"])
-        song8 = Song(songs[7]["title"], songs[7]["by"])
+        self.song1 = Song(songs[0]["title"], songs[0]["by"])
+        self.song2 = Song(songs[1]["title"], songs[1]["by"])
+        self.song3 = Song(songs[2]["title"], songs[2]["by"])
+        self.song4 = Song(songs[3]["title"], songs[3]["by"])
+        self.song5 = Song(songs[4]["title"], songs[4]["by"])
+        self.song6 = Song(songs[5]["title"], songs[5]["by"])
+        self.song7 = Song(songs[6]["title"], songs[6]["by"])
+        self.song8 = Song(songs[7]["title"], songs[7]["by"])
         
-        songlist1 = [song1, song2, song3, song4, song5]
-        songlist2 = [song6, song7, song8]
+        self.songlist1 = [self.song1, self.song2, self.song3, self.song4, self.song5]
+        self.songlist2 = [self.song6, self.song7, self.song8]
 
-        guest1 = Guest(guests[0])
-        guest2 = Guest(guests[0])
-        guest3 = Guest(guests[0])
-        guest4 = Guest(guests[0])
-        guest5 = Guest(guests[0])
-        guest6 = Guest(guests[0])
-        guest7 = Guest(guests[0])
-        guest8 = Guest(guests[0])
-        guest9 = Guest(guests[0])
-        guest10 = Guest(guests[0])
-        guest11 = Guest(guests[0])
+        self.guest1 = Guest(guests[0])
+        self.guest2 = Guest(guests[0])
+        self.guest3 = Guest(guests[0])
+        self.guest4 = Guest(guests[0])
+        self.guest5 = Guest(guests[0])
+        self.guest6 = Guest(guests[0])
+        self.guest7 = Guest(guests[0])
+        self.guest8 = Guest(guests[0])
+        self.guest9 = Guest(guests[0])
+        self.guest10 = Guest(guests[0])
+        self.guest11 = Guest(guests[0])
 
-        self.guestlist1 = [guest1, guest2, guest3, guest4, guest5]
-        self.guestlist2 = [guest6, guest7, guest8]
+        self.guestlist1 = [self.guest1, self.guest2, self.guest3, self.guest4, self.guest5]
+        self.guestlist2 = [self.guest6, self.guest7, self.guest8]
 
-        self.room1 = Room(room_names[0], self.guestlist1, songlist1)
-        self.room2 = Room(room_names[1], [], songlist2)
+        self.room1 = Room(room_names[0], self.guestlist1, self.songlist1)
+        self.room2 = Room(room_names[1], [], self.songlist2)
 
     def test_room_has_name(self):
         self.assertEqual(self.room1.name, room_names[0])
@@ -87,4 +87,35 @@ class TestRoom(unittest.TestCase):
     def test_room_has_songs(self):
         self.assertTrue(len(self.room2.songlist) > 0)
 
+    def test_check_in_1_guest(self):
+        self.assertEqual(len(self.room1.guestlist), 5)
+        self.assertEqual(len(self.room2.guestlist), 0)
+        self.room1.check_in_1_guest(self.guest9)
+        self.room2.check_in_1_guest(self.guest10)
+        self.assertEqual(len(self.room1.guestlist), 6)
+        self.assertEqual(len(self.room2.guestlist), 1)
+
+    def test_check_out_1_guest_ok(self):
+        self.assertEqual(len(self.room1.guestlist), 5)
+        self.room1.check_out_1_guest(self.guest3)
+        self.assertEqual(len(self.room1.guestlist), 4)
     
+    def test_check_out_1_guest_empty_room(self):
+        self.assertEqual(len(self.room2.guestlist), 0)
+        result = self.room2.check_out_1_guest(self.guest10)
+        self.assertEqual(result, "this room is empty")
+
+    def test_check_out_1_guest_is_not_here(self):
+        self.assertEqual(len(self.room1.guestlist), 5)
+        result = self.room1.check_out_1_guest(self.guest10)
+        self.assertEqual(result, "guest not in this room")
+
+    def test_check_out_all_guests_in_this_room(self):
+        self.assertEqual(len(self.room1.guestlist), 5)
+        self.room1.check_out_all_guests_in_this_room()
+        self.assertEqual(len(self.room1.guestlist), 0)
+
+    def test_check_out_all_guests_in_empty_room(self):
+        self.assertEqual(len(self.room2.guestlist), 0)
+        result = self.room2.check_out_all_guests_in_this_room()
+        self.assertEqual(result, "this room is already empty")
